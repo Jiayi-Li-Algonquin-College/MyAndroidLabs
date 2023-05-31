b9859d3727b27783301c70770a53d60b7462777d
Yanxiang Liang <lian0094@algonquinlive.com>
2023-05-31 10:33:45 -0400

    hello

---
+++ app/src/main/java/algonquin/cst2335/torunse/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.torunse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
