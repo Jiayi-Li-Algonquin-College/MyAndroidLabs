37b767cfada18ffa0fd2085b8354e19abbbb7a4e
Yanxiang Liang <lian0094@algonquinlive.com>
2023-05-31 13:39:41 -0400

    hello

---
+++ app/src/main/java/algonquin/cst2335/torunse/SecondActivity.java
@@ -0,0 +1,19 @@
+package algonquin.cst2335.torunse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+
+
+    }
+}
