02f4a022516f882b3d80ca9da29ef3b64f28fc8b
Yanxiang Liang <lian0094@algonquinlive.com>
2023-06-07 12:05:38 -0400

    hello

--- app/src/main/java/algonquin/cst2335/torunse/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/torunse/MainActivity.java
@@ -8,21 +8,40 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * MainActivity class represents an Android activity that allows the user to enter and check a password.
+ * It uses various methods to validate the password complexity and display feedback messages.
+ *
+ * @author Jiayi Li
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**This holds the text at the center of the screen*/
+    private TextView tv = null;
+    /**This holds the password that users entered at the bottom of text*/
+    private EditText et = null;
+    /**This holds button the at the bottom of the screen*/
+    private Button btn = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView tv = findViewById(R.id.textView);
-        EditText et = findViewById(R.id.myEditText);
-        Button btn = findViewById(R.id.loginBtn);
+        tv = findViewById(R.id.textView);
+        et = findViewById(R.id.myEditText);
+        btn = findViewById(R.id.loginBtn);
 
         btn.setOnClickListener( clk ->{
             String password=et.getText().toString();
-            checkPasswordComplexity( password );
+            if(checkPasswordComplexity( password )){
+                tv.setText("Your password meets the requirements");
+            }else{
+                tv.setText("You shall not pass!");
+            }
         });
+
+
+
+
     }
 
     /**
